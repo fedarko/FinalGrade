cde688ea402573cd4ded47a93495845e015b200e
Marcus Fedarko <marcus.fedarko@gmail.com>
2013-05-25 20:49:10 -0400

    V1.1

--- src/com/mfedarko/finalgrade/MainActivity.java
+++ src/com/mfedarko/finalgrade/MainActivity.java
@@ -1,7 +1,27 @@
+/* Copyright 2013 Marcus Fedarko
+ * Contact Email: marcus.fedarko@gmail.com
+ * 
+ * This file is part of FinalGrade.
+ * 
+ *     FinalGrade is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FinalGrade is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FinalGrade.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.mfedarko.finalgrade;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -41,7 +61,8 @@ public class MainActivity extends Activity {
     
     public void calculateFinalGrade(View view) {
         int i;
-        int qualitypts = 0;
+        double qualitypts = 0;
+        double exam_avg = 0;
         boolean invalidInput = false;
         String finalLetterGrade;
         
@@ -50,23 +71,23 @@ public class MainActivity extends Activity {
         List<String> quarterGrades = new ArrayList<String>();
         
         for(i = 0; i <= 3; i++) {
-                quarterGrades.add(quarters[i].getText().toString().toLowerCase());
+                quarterGrades.add(quarters[i].getText().toString().toLowerCase(Locale.getDefault()));
         }
         
         // Next, add points to the total quality point count for each quarter.
         for(i = 0; i <= 3; i++) {
                 String g = quarterGrades.get(i);
                 if(g.equals("a")) {
-                        qualitypts += 8;
-                }
-                else if(g.equals("b")) {
-                        qualitypts += 6;
-                }
-                else if(g.equals("c")) {
                         qualitypts += 4;
                 }
-                else if(g.equals("d")) {
+                else if(g.equals("b")) {
+                        qualitypts += 3;
+                }
+                else if(g.equals("c")) {
                         qualitypts += 2;
+                }
+                else if(g.equals("d")) {
+                        qualitypts += 1;
                 }
                 else {
                         // An E = +/- 0 quality points, so we just ignore it.
@@ -79,22 +100,22 @@ public class MainActivity extends Activity {
         }
         if(!invalidInput) {
                 // Done adding points from quarters. Now add points from exams.
-                String mg = midterm.getText().toString().toLowerCase();
-                String fg = fin.getText().toString().toLowerCase();
+                String mg = midterm.getText().toString().toLowerCase(Locale.getDefault());
+                String fg = fin.getText().toString().toLowerCase(Locale.getDefault());
                 String[] exams = {mg, fg};
                 
                 for(i = 0; i < 2; i++) {
                         if(exams[i].equals("a")) {
-                                qualitypts += 4;
+                                exam_avg += 4;
                         }
                         else if(exams[i].equals("b")) {
-                                qualitypts += 3;
+                                exam_avg += 3;
                         }
                         else if(exams[i].equals("c")) {
-                                qualitypts += 2;
+                                exam_avg += 2;
                         }
                         else if(exams[i].equals("d")) {
-                                qualitypts += 1;
+                                exam_avg += 1;
                         }
                         else {
                                 if(!(exams[i].equals("e"))) {
@@ -105,22 +126,26 @@ public class MainActivity extends Activity {
                         }
                 }
                 if(!invalidInput) {
-                        if(qualitypts >= 35) {
+                		exam_avg /= 2.0;
+                		qualitypts += exam_avg;
+                		
+                		qualitypts /= 5.0;
+                        if(qualitypts >= 3.5) {
                                 finalLetterGrade = "A";
                         }
-                        else if(qualitypts >= 30) {
+                        else if(qualitypts >= 2.5) {
                                 finalLetterGrade = "B";
                         }
-                        else if(qualitypts >= 25) {
+                        else if(qualitypts >= 1.5) {
                                 finalLetterGrade = "C";
                         }
-                        else if(qualitypts >= 20) {
+                        else if(qualitypts >= 0.75) {
                                 finalLetterGrade = "D";
                         }
                         else {
                                 finalLetterGrade = "E";
                         }
-                        results.setText(String.format("Final Grade: %d quality points / 40 possible = %s", qualitypts, finalLetterGrade));
+                        results.setText(String.format("Final Grade: %.2f quality points / 4.00 possible = %s", qualitypts, finalLetterGrade));
                 }
         }
     }
