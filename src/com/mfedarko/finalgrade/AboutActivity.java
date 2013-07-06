fbf235ecc1858d9d77b5ef7cff3b8f2f1aad0f88
Marcus Fedarko <marcus.fedarko@gmail.com>
2013-07-06 03:13:41 -0400

    Added GPL notice to AboutActivity; I initially forgot to

--- src/com/mfedarko/finalgrade/AboutActivity.java
+++ src/com/mfedarko/finalgrade/AboutActivity.java
@@ -1,3 +1,22 @@
+/* Copyright 2013 Marcus Fedarko
+ * Contact Email: marcus.fedarko@gmail.com
+ * 
+ * This file is part of FinalGrade.
+ * 
+ *     FinalGrade is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FinalGrade is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FinalGrade.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.mfedarko.finalgrade;
 
 import android.net.Uri;
